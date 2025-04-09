0ddaee4b4e2b55c3345064363bfba825c5762504
Robert Fornof <rob@fornof.me>
2025-04-08 21:48:53 -0600

    build sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-# GOOS=js GOARCH=wasm go build -o chords.wasm main.go
+output_dir=${1:-"../music-central-web/public"}
+echo 'output main.wasm to' $output_dir
 tinygo build -o=chords.wasm -target=wasm -no-debug ./wasm.go
  mv chords.wasm "../music-central-web/public"
